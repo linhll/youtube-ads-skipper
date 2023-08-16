ef184c41c4dadd70b2bb7a1150cf57cc4a0baca5
linhnh <nhlinhcs@gmail.com>
2023-08-16 16:07:13 +0700

    update skipper script

--- skipper.js
+++ skipper.js
@@ -10,60 +10,33 @@
 // ==/UserScript==
 
 (function () {
-  "use strict";
-  const adsContainerNode = document.getElementsByClassName(
-    "video-ads ytp-ad-module"
-  )[0];
-  if (adsContainerNode) {
-    const config = { subtree: true, childList: true };
-    const callback = (mutationList, observer) => {
-      let playButton = document.querySelector("button.ytp-play-button");
-      if (adsContainerNode.childNodes.length === 0) {
-        console.log("ads skipped");
-        if (
-        playButton?.getAttribute("data-title-no-tooltip")?.toLowerCase() ===
-        "play"
-      ) {
-        playButton.click();
-      }
-        return;
-      }
-
-      if (
-        playButton?.getAttribute("data-title-no-tooltip")?.toLowerCase() ===
-        "play"
-      ) {
-        playButton.click();
-      }
-       if(!Array.isArray(mutationList)){
-         return;
-       }
-      for (const mutation of mutationList) {
-        if (mutation.type == "childList") {
-          let button = adsContainerNode.querySelector(
-            "button.ytp-ad-skip-button.ytp-button"
-          );
-          if (button) {
-            console.log("legacy button detected");
-            button.click();
-            button = undefined;
-          }
-          button = adsContainerNode.querySelector(
-            "button.ytp-ad-skip-button-modern.ytp-button"
-          );
-          if (button) {
-            console.log("modern button detected");
-            button.click();
-            button = undefined;
-          }
-        }
-      }
-    };
-    const observer = new MutationObserver(callback);
-
-    // Start observing the target node for configured mutations
-    observer.observe(adsContainerNode, config);
-    window.setTimeout(callback, 3000);
-
+  'use strict';
+  function getPlayer() {
+    return document.getElementById('ytd-player')?.getPlayer();
   }
+  let _t = setInterval(() => {
+    const adsContainerNode = document.getElementsByClassName(
+      'video-ads ytp-ad-module'
+    )[0];
+    if (adsContainerNode) {
+      const config = { subtree: true, childList: true };
+      const callback = (mutationList, observer) => {
+        if (adsContainerNode.childNodes.length === 0) {
+          getPlayer()?.playVideo();
+          console.log('ads skipped');
+
+          return;
+        }
+        console.log('skip ads');
+        getPlayer().cancelPlayback();
+      };
+
+      const observer = new MutationObserver(callback);
+
+      // Start observing the target node for configured mutations
+      observer.observe(adsContainerNode, config);
+      window.setTimeout(callback, 3000);
+      window.clearInterval(_t);
+    }
+  }, 1000);
 })();
