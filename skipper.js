dfe6cc2587f9b788b5572c98eb5692f764b8d2cb
linhnh <nhlinhcs@gmail.com>
2023-10-12 18:16:53 +0700

    update ads skiper script

--- skipper.js
+++ skipper.js
@@ -14,21 +14,19 @@
   function getPlayer() {
     return document.getElementById('ytd-player')?.getPlayer();
   }
-  let _t = setInterval(() => {
+  let t_auto = setInterval(() => {
     const adsContainerNode = document.getElementsByClassName(
       'video-ads ytp-ad-module'
     )[0];
     if (adsContainerNode) {
       const config = { subtree: true, childList: true };
-      const callback = (mutationList, observer) => {
+      const callback = () => {
         if (adsContainerNode.childNodes.length === 0) {
           getPlayer()?.playVideo();
-          console.log('ads skipped');
+          console.log('ytadskiper', 'ads skipped');
 
           return;
         }
-        console.log('skip ads');
-        getPlayer().cancelPlayback();
       };
 
       const observer = new MutationObserver(callback);
@@ -36,7 +34,29 @@
       // Start observing the target node for configured mutations
       observer.observe(adsContainerNode, config);
       window.setTimeout(callback, 3000);
-      window.clearInterval(_t);
+      window.clearInterval(t_auto);
     }
   }, 1000);
+
+  let t_skip = setInterval(() => {
+    const html5PlayerNode = document.getElementById('movie_player');
+
+    if (html5PlayerNode) {
+      const config = { attributes: true };
+      const callback = () => {
+        if (html5PlayerNode.classList.contains('ad-showing')) {
+          console.log('ytadskiper', 'skip ads');
+          getPlayer().cancelPlayback();
+          return;
+        }
+      };
+
+      const observer = new MutationObserver(callback);
+
+      // Start observing the target node for configured mutations
+      observer.observe(html5PlayerNode, config);
+      window.setTimeout(callback, 3000);
+      window.clearInterval(t_skip);
+    }
+  });
 })();
