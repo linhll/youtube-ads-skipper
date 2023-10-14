4fe53770269a85d6cf5a844d87dfef28c4f48436
linhll <31204093+linhll@users.noreply.github.com>
2023-10-14 12:33:16 +0700

    Update skipper.js

--- skipper.js
+++ skipper.js
@@ -1,62 +1,68 @@
 // ==UserScript==
-// @name         Auto skip youtube ads
+// @name         Youtube ads
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       https://github.com/linhll
 // @match        https://www.youtube.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  'use strict';
-  function getPlayer() {
-    return document.getElementById('ytd-player')?.getPlayer();
-  }
-  let t_auto = setInterval(() => {
-    const adsContainerNode = document.getElementsByClassName(
-      'video-ads ytp-ad-module'
-    )[0];
-    if (adsContainerNode) {
-      const config = { subtree: true, childList: true };
-      const callback = () => {
-        if (adsContainerNode.childNodes.length === 0) {
-          getPlayer()?.playVideo();
-          console.log('ytadskiper', 'ads skipped');
-
-          return;
-        }
-      };
-
-      const observer = new MutationObserver(callback);
-
-      // Start observing the target node for configured mutations
-      observer.observe(adsContainerNode, config);
-      window.setTimeout(callback, 3000);
-      window.clearInterval(t_auto);
+    'use strict';
+    function getPlayer() {
+        return document.getElementById('ytd-player')?.getPlayer();
     }
-  }, 1000);
 
-  let t_skip = setInterval(() => {
-    const html5PlayerNode = document.getElementById('movie_player');
+    let t_skip = setInterval(() => {
+        const html5PlayerNode = document.getElementById('movie_player');
 
-    if (html5PlayerNode) {
-      const config = { attributes: true };
-      const callback = () => {
-        if (html5PlayerNode.classList.contains('ad-showing')) {
-          console.log('ytadskiper', 'skip ads');
-          getPlayer().cancelPlayback();
-          return;
+        if (html5PlayerNode) {
+            const config = { attributes: true };
+            const callback = () => {
+                const player = getPlayer();
+                if (!player) {
+                    return;
+                }
+                if (html5PlayerNode.classList.contains('ad-showing')) {
+                    console.log('ytadskiper', 'skip ads');
+                    getPlayer().cancelPlayback();
+                }
+
+                if (window.yt) {
+                    window.yt.ads = {};
+                    window.yt.ads_ = {};
+                    window.yt.www.ads = {}
+                }
+
+                if (ytads) {
+                    ytads.bulleit = {}
+                }
+
+                if (player._customized) {
+                    return;
+                }
+                player._customized = true;
+                player._getPlayerResponse = player.getPlayerResponse;
+                player.getPlayerResponse = function () {
+                    const obj = player._getPlayerResponse();
+                    obj.adPlacements.length = 0
+                    obj.adBreakHeartbeatParams = '';
+                    obj.playerAds.length = 0;
+                    obj.adSlots.length = 0;
+                    return obj;
+                }
+
+
+            };
+
+            const observer = new MutationObserver(callback);
+
+            // Start observing the target node for configured mutations
+            observer.observe(html5PlayerNode, config);
+            window.setTimeout(callback, 3000);
+            window.clearInterval(t_skip);
         }
-      };
-
-      const observer = new MutationObserver(callback);
-
-      // Start observing the target node for configured mutations
-      observer.observe(html5PlayerNode, config);
-      window.setTimeout(callback, 3000);
-      window.clearInterval(t_skip);
-    }
-  });
+    });
 })();
